873ff1bb072f6c71a86ea8fd584f462e43710afc
Benjamin Blott <benjaminblott@verticalonellc.com>
2018-02-20 14:10:42 -0600

    Create scraper.R

---
+++ scraper.R
@@ -0,0 +1,69 @@
+#libraries
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(readxl)
+
+#read in events list
+#all_events_list <- readRDS("C:/Users/Ben/Desktop/R Projects/olympics/all_events_2018.rds")
+
+#global vars
+hilo_cols <- 2
+
+#Function for scrapign nicerodds website
+nicerodds_scraper <- function(url, hilo_cols = 2) {
+  
+  #read in and save url
+  webpage <- read_html(url)
+  
+  #read in event
+  event <- html_nodes(webpage, '.subItemSelected')
+  event <- html_text(event)
+  
+  #headers
+  event_headers <- html_nodes(webpage, 'th')  #read in 'headers' node
+  event_headers <- html_text(event_headers)  #extract
+  event_headers <- data.frame(matrix(unlist(event_headers), nrow = 1), stringsAsFactors = FALSE)  #convert to df
+  
+  names(event_headers) <- event_headers[1,]  #set row 1 as column names
+  colnames(event_headers)[1] <- "name"  #rename first column to team/athlete name
+  columns <- ncol(event_headers)  #save number of columns
+  
+  #names
+  event_names <- html_nodes(webpage, '#mainplh_boAutoOddsTable1_GridView1 td:nth-child(1)')
+  event_names <- html_text(event_names)
+  event_names <- as.data.frame(event_names)
+  
+  event_names[,1] <- gsub("\\(.*","", event_names[,1])  #remove country abbreviations and parenthesis from name col
+  event_names[,1] <- sub("\\s+$", "", event_names[,1])  #remove trailing white space
+  
+  #odds hi lo
+  event_hilo <- html_nodes(webpage, '#mainplh_boAutoOddsTable1_GridView1 td+ td')
+  event_hilo <- html_text(event_hilo)
+  event_hilo <- data.frame(matrix(as.numeric(unlist(event_hilo)), ncol = hilo_cols, byrow = TRUE))
+  
+  #odds sites
+  event_odds <- html_nodes(webpage, '#mainplh_boAutoOddsTable1_GridView2 td')
+  event_odds <- html_text(event_odds)
+  event_odds <- data.frame(matrix(as.numeric(unlist(event_odds)), ncol = columns - 3, byrow = TRUE))
+  
+  #bind columns
+  event_df <- event_names %>%
+    bind_cols(event_hilo) %>%
+    bind_cols(event_odds) %>%
+    head(-1)
+  
+  #will need to do something with duplicate names similar to what I did with baseball
+  
+  #name cols
+  names(event_df) <- names(event_headers)  #convert to headers df names
+  
+  #add event col
+  event_df$event <- event
+  
+  #add ifelse() for event that there are no odds posted
+  
+  #return event df
+  event_df
+}
